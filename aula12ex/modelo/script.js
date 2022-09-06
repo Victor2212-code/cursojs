ec6858b2b86064d70a7c8230dedeb9334af0c095
Victor2212-code <110578804+Victor2212-code@users.noreply.github.com>
2022-09-06 20:54:25 -0300

    alteração

--- aula12ex/modelo/script.js
+++ aula12ex/modelo/script.js
@@ -1,1 +1,22 @@
-window.alert('Olá')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('img')
+    var data = new Date()
+    var hora = data.getHours()
+    var hora = 21
+    msg.innerHTML = `Agora são ${hora} horas`
+    if(hora >= 0 && hora <=12){
+        //Bom dia
+        img.src = 'fotoamanha.png' 
+        document.body.style.background = '#ef853b'
+    }
+    else if ( hora >=12 && hora < 18){
+        //Boa tarde
+        img.src = 'fototarde.png'
+        document.body.style.background = '#d63c00'
+    }else{
+        //Boa Noite
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#3b2d47'
+    }
+}
